770eee25787679f6eeb1418198269a4d54ceb539
Petr Arsentev <parsentev@yandex.ru>
2020-03-18 07:22:11 +0300

    Add test for Logic

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -3,11 +3,6 @@ package ru.job4j.chess.firuges.black;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
-/**
- * @author Petr Arsentev (parsentev@yandex.ru)
- * @version $Id$
- * @since 0.1
- */
 public class BishopBlack implements Figure {
     private final Cell position;
 
@@ -25,23 +20,9 @@ public class BishopBlack implements Figure {
         throw new IllegalStateException(
                 String.format("Could not way by diagonal from %s to %s", source, dest)
         );
-//        if (!isDiagonal(source, dest)) {
-//            throw new IllegalStateException(
-//                    String.format("Could not way by diagonal from %s to %s", source, dest)
-//            );
-//        }
-//        int size = ...;
-//        Cell[] steps = new Cell[size];
-//        int deltaX = ...;
-//        int deltaY = ...;
-//        for (int index = 0; index < size; index++) {
-//            steps[index] = ...
-//        }
-//        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        //TODO check diagonal
         return false;
     }
 
---
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.chess;
+
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class LogicTest {
+
+    @Test
+    public void move() {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        boolean rsl = logic.move(Cell.C1, Cell.H6);
+        assertThat(rsl, is(true));
+    }
+}
