41af5ab9b0bff37688f1f37c30f78606efc9266d
Petr Arsentev <parsentev@yandex.ru>
2020-12-07 07:47:57 +0300

    fix test data in the puzzle project

--- chess/src/main/java/ru/job4j/chess/ImpossibleMoveException.java
+++ chess/src/main/java/ru/job4j/chess/ImpossibleMoveException.java
@@ -1,4 +1,7 @@
 package ru.job4j.chess;
 
-public class ImpossibleMoveException extends Exception {
+public class ImpossibleMoveException extends RuntimeException {
+    public ImpossibleMoveException(String message) {
+        super(message);
+    }
 }
--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -1,5 +1,6 @@
 package ru.job4j.chess.firuges.black;
 
+import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
@@ -17,7 +18,7 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell dest) {
-        throw new IllegalStateException(
+        throw new ImpossibleMoveException(
                 String.format("Could not way by diagonal from %s to %s", position, dest)
         );
     }
