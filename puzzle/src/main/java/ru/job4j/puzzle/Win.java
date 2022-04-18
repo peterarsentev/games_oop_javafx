c0321fa491b4b188bd4e31b7276c34f6b060421e
d.lebedev <d.lebedev@rabota.ru>
2022-04-18 22:39:12 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,36 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
+        return checkHLine(board) && checkVLine(board);
+    }
+
+    public static boolean checkHLine(int[][] board) {
         boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][0] == 1) {
+                for (int j = 1; j < board.length; j++) {
+                    if (board[i][j] != 1) {
+                        rsl = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean checkVLine(int[][] board){
+        boolean rsl = true;
+        for (int j = 0; j < board.length; j++) {
+            if (board[0][j] == 1) {
+                for (int i = 1; i < board.length; i++) {
+                    if (board[i][j] != 1) {
+                        rsl = false;
+                        break;
+                    }
+                }
+            }
+        }
         return rsl;
     }
 }
--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-@Ignore
 public class WinTest {
     @Test
     public void whenVerticalWin() {
