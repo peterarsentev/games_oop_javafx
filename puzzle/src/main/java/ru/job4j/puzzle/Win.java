7f2eb8a0be11eed980e1b886bb7f06ee763518b3
d.lebedev <d.lebedev@rabota.ru>
2022-04-19 22:52:47 +0300

    2. Сокобан. Тестовое задание. [#75283] add find line

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,10 +2,11 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        return checkHLine(board) && checkVLine(board);
+        /*return checkHLine(board) && checkVLine(board);*/
+        return monoHLine(board, findHLine(board)) || monoVLine(board, findVLine(board));
     }
 
-    public static boolean checkHLine(int[][] board) {
+    /*public static boolean checkHLine(int[][] board) {
         boolean rsl = true;
         for (int i = 0; i < board.length; i++) {
             if (board[i][0] == 1) {
@@ -18,9 +19,9 @@ public class Win {
             }
         }
         return rsl;
-    }
+    }*/
 
-    public static boolean checkVLine(int[][] board) {
+    /*public static boolean checkVLine(int[][] board) {
         boolean rsl = true;
         for (int j = 0; j < board.length; j++) {
             if (board[0][j] == 1) {
@@ -30,6 +31,50 @@ public class Win {
                         break;
                     }
                 }
+            }
+        }
+        return rsl;
+    }*/
+
+    public static int findHLine(int[][] board) {
+        int rsl = 0;
+        for (int i = 0; i < board.length; i++) {
+            if(board[i][0] == 1) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static int findVLine(int[][] board) {
+        int rsl = 0;
+        for (int i = 0; i < board.length; i++) {
+            if(board[0][i] == 1) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean monoHLine(int[][] board, int hLine) {
+        boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[hLine][i] != 1) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean monoVLine(int[][] board, int vLine) {
+        boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][vLine] != 1) {
+                rsl = false;
+                break;
             }
         }
         return rsl;
