f27e0c2f5cfffc5279bb3319006f490833d7252c
d.lebedev <d.lebedev@rabota.ru>
2022-04-20 20:19:12 +0300

    2. Сокобан. Тестовое задание. [#75283] fix2

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,56 +2,10 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        /*return checkHLine(board) && checkVLine(board);*/
-        return monoHLine(board, findHLine(board)) || monoVLine(board, findVLine(board));
-    }
-
-    /*public static boolean checkHLine(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][0] == 1) {
-                for (int j = 1; j < board.length; j++) {
-                    if (board[i][j] != 1) {
-                        rsl = false;
-                        break;
-                    }
-                }
-            }
-        }
-        return rsl;
-    }*/
-
-    /*public static boolean checkVLine(int[][] board) {
-        boolean rsl = true;
-        for (int j = 0; j < board.length; j++) {
-            if (board[0][j] == 1) {
-                for (int i = 1; i < board.length; i++) {
-                    if (board[i][j] != 1) {
-                        rsl = false;
-                        break;
-                    }
-                }
-            }
-        }
-        return rsl;
-    }*/
-
-    public static int findHLine(int[][] board) {
-        int rsl = 0;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][0] == 1) {
-                rsl = i;
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    public static int findVLine(int[][] board) {
-        int rsl = 0;
-        for (int i = 0; i < board.length; i++) {
-            if (board[0][i] == 1) {
-                rsl = i;
+            if (monoHLine(board, i) || monoVLine(board, i)) {
+                rsl = true;
                 break;
             }
         }
