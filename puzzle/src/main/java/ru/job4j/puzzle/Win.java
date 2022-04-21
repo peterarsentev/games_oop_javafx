2574f3dd2f3ba2714f246a3c3ae536f1cbdada03
d.lebedev <d.lebedev@rabota.ru>
2022-04-21 20:15:14 +0300

    2. Сокобан. Тестовое задание. [#75283 #290758] fix 3

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,7 +4,7 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (monoHLine(board, i) || monoVLine(board, i)) {
+            if (board[i][i] == 1 && monoHLine(board, i) || monoVLine(board, i)) {
                 rsl = true;
                 break;
             }
