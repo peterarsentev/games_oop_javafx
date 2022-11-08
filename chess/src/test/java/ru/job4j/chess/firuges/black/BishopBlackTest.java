ae81aa03ed82a8df4ef030a3495fac27b41be219
Denis Lazykin <dlazykin856@gmail.com>
2022-11-08 10:57:33 +0300

    2. Каркас шахматной доски [#792 #257891].Добавил тест метода position(); из класса BishopBlack

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -7,5 +7,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 class BishopBlackTest {
-
+    /**
+     * Создали объект слона
+     * вызвали у него метод position.
+     * проверили, что он занимает ту же ячейку, что и при создании объекта.
+     */
+    @Test
+    public void whenCorrectPosition() {
+        Cell start = Cell.A1;
+        BishopBlack bishopBlack = new BishopBlack(start);
+        assertThat(bishopBlack.position(), is(start));
+    }
 }
