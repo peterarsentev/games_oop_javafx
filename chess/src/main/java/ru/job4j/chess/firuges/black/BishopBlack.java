53946af28fd6a0a30eea256553167fd925476762
Denis Lazykin <dlazykin856@gmail.com>
2022-11-08 11:34:22 +0300

    2. Каркас шахматной доски [#792 #257891].Добавил тест метод way(); из класса BishopBlack и на писал на него тест метод

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -16,15 +16,33 @@ public class BishopBlack implements Figure {
         return position;
     }
 
+    /**
+     * Строит путь от начальной точки до конечной
+     * @param dest до куда идет фигура
+     * @return шаги до точки
+     * @throws ImpossibleMoveException исключение, если пользователь сделал неверный ход
+     */
     @Override
-    public Cell[] way(Cell dest) {
-        throw new ImpossibleMoveException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
-    }
-
-    public boolean isDiagonal(Cell source, Cell dest) {
-        return false;
+    public Cell[] way(Cell dest) throws ImpossibleMoveException {
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not way by diagonal from %s to %s", position, dest)
+            );
+        }
+        int positionX = position().getX();
+        int positionY = position().getY();
+        int destX = dest.getX();
+        int destY = dest.getY();
+        int size = Math.abs(positionX - destX);
+        Cell[] steps = new Cell[size];
+        int deltaX = positionX - destX > 0 ? -1 : 1;
+        int deltaY = positionY - destY > 0 ? -1 : 1;
+        for (int index = 0; index < size; index++) {
+            positionX += deltaX;
+            positionY += deltaY;
+            steps[index] = Cell.findBy(positionX, positionY);
+        }
+        return steps;
     }
 
     @Override
--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,7 +1,11 @@
 package ru.job4j.chess.firuges.black;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
+
+import java.security.cert.CRL;
+import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -28,5 +32,18 @@ class BishopBlackTest {
         Cell step = Cell.A2;
         BishopBlack bishopBlack = new BishopBlack(Cell.A2);
         assertThat(bishopBlack.copy(step).position(), is(step));
+    }
+
+    /**
+     * Создали объект с начальным положением C1. Вызываем метод way G5.
+     * проверям возвтрат массивов из четырех клеток: D2, E3, F4, G5.
+     * @throws ImpossibleMoveException исключение если позьзователь сделал неверный ход
+     */
+    @Test
+    public void whenCorrectWay() throws ImpossibleMoveException {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] way = bishopBlack.way(Cell.G5);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(way, is(expected));
     }
 }
